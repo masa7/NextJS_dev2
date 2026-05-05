5bac7d1ed51fe5ab05c9f35f29ceb6730aaddc12
masa7 <sw3nyc0043@gmail.com>
2026-05-05 09:26:16 +0900

    add searchBox

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,36 @@
+import { getPosts, searchPosts } from "@/lib/post"
+import PostCard from "@/components/post/PostCard"
+import { Post } from "@/types/post"
+import { SearchParams } from "next/dist/server/request/search-params"
+
+type searchParams = {
+  search? : string
+}
+
+export default async function PostsPage(
+  {searchParams}:{searchParams: Promise<SearchParams>}) {
+  
+  const resolvedSearchParams = await searchParams
+  //const query = resolvedSearchParams.search || ''
+
+  // 配列が返ってきた場合はスペースで結合し、そうでなければ文字列として扱う
+  const rawQuery = resolvedSearchParams.search
+  const query = Array.isArray(rawQuery) ? rawQuery.join(' ') : (rawQuery || '')
+  
+  const posts = query 
+  ? await searchPosts(query) as Post[]
+  : await getPosts() as Post[]
+  //const posts = await getPosts() as Post[]
+  
+  return (
+    <>
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {posts.map((post)=>(
+                <PostCard key={post.id} post={post} />
+            ))}
+        </div>
+    </div>
+    </>
+  )
+}
