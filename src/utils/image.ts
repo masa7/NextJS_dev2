062bc5d5e3d82ef1379a2fa854deff30fb36866b
masa7 <sw3nyc0043@gmail.com>
2026-05-06 22:09:22 +0900

    add supabase setting

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,22 +1,9 @@
 import { Buffer } from 'node:buffer';
 import { writeFile } from 'fs/promises'
 import path from 'path'
-//import { supabase } from '@/lib/supabase';
+import { supabase } from '@/lib/supabase';
 
 export async function saveImage(file: File): Promise<string | null>{
-  const buffer = Buffer.from(await file.arrayBuffer()); // バイナリデータをBuﬀerに変換
-  const fileName = `${Date.now()}_${file.name}`; // ファイル名生成 日時_ファイル名
-  const uploadDir = path.join(process.cwd(), "public/images"); // アップロードフォルダ
-  try {
-    const filePath = path.join(uploadDir, fileName); // 保存先の完全なファイル名
-    await writeFile(filePath, buffer); // 指定パスにファイル(buﬀer)を書き込む
-    return `/images/${fileName}`; // URLパスを返す
-  } catch (error) {
-    console.error("画像保存エラー:", error);
-    return null;
-  }
-  
-  /*
   const useSupabase = process.env.NEXT_PUBLIC_USE_SUPABASE_STORAGE === 'true';
 
   if (useSupabase) {
@@ -24,7 +11,6 @@ export async function saveImage(file: File): Promise<string | null>{
   } else {
     return await saveImageToLocal(file);
   }
-  */
 }
 
 export async function saveImageToLocal(file: File): Promise<string | null>{
@@ -42,11 +28,10 @@ export async function saveImageToLocal(file: File): Promise<string | null>{
   }
 }
 
-/*
 async function saveImageToSupabase(file: File): Promise<string | null> {
   const fileName = `${Date.now()}_${file.name}`;
   const { error } = await supabase.storage
-    .from('udemy_next_blog_bucket')
+    .from('blog_bucket')
     .upload(fileName, file, {
         cacheControl: '3600',
         upsert: false,
@@ -57,9 +42,8 @@ async function saveImageToSupabase(file: File): Promise<string | null> {
     return null;   }
 
   const { data: publicUrlData } = supabase.storage
-    .from('udemy_next_blog_bucket')
+    .from('blog_bucket')
     .getPublicUrl(fileName);
 
   return publicUrlData.publicUrl;  
 }
-*/
